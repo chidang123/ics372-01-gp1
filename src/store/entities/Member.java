d0901e67c93185261a00c9ca9648281b0920170e
Andrew Vick <atvick21@gmail.com>
2021-03-18 15:24:08 -0500

    Use case 1 and 2 are done.
    Commented all of my code and finished Use case 1 and 2

--- src/store/entities/Member.java
+++ src/store/entities/Member.java
@@ -5,6 +5,12 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 
+/**
+ * Member represents a member of the store
+ * 
+ * @author Andrew Vick
+ *
+ */
 public class Member implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String id;
@@ -12,11 +18,17 @@ public class Member implements Serializable {
 	private String address;
 	private Calendar dateJoined;
 	private String phoneNumber;
-	private boolean feePaid;
 	private static final String MEMBER_STRING = "XM";
 	private static int idCounter;
 	private TransactionList membersTransactions = new TransactionList();
 
+	/**
+	 * Creates a single member
+	 * 
+	 * @param name        The name of the member
+	 * @param address     The address of the member
+	 * @param phoneNumber The phone number of hte member
+	 */
 	public Member(String name, String address, String phoneNumber) {
 		this.name = name;
 		this.address = address;
@@ -25,50 +37,92 @@ public class Member implements Serializable {
 		id = MEMBER_STRING + ++idCounter;
 	}
 
+	/**
+	 * Getter for the Id
+	 * 
+	 * @return Member's Id
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Getter for the member's name
+	 * 
+	 * @return member's name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Setter for member's name
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Getter for member's address
+	 * 
+	 * @return member's address
+	 */
 	public String getAddress() {
 		return address;
 	}
 
+	/**
+	 * Setter for member's address
+	 * 
+	 * @param address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+	/**
+	 * Getter for the date the member joined the store
+	 * 
+	 * @return date the member joined the store
+	 */
 	public Calendar getDateJoined() {
 		return dateJoined;
 	}
 
+	/**
+	 * Getter for member's phone number
+	 * 
+	 * @return member's phone number
+	 */
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
+	/**
+	 * Setter for member's phone number
+	 * 
+	 * @param phoneNumber
+	 */
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
-	public boolean isFeePaid() {
-		return feePaid;
-	}
-
-	public void setFeePaid(boolean feePaid) {
-		this.feePaid = feePaid;
-	}
-
+	/**
+	 * Adds a transaction to the transaction collection
+	 * 
+	 * @param transaction
+	 */
 	public void addMemberTransaction(Transaction transaction) {
 		membersTransactions.addTransaction(transaction);
 	}
 
+	/**
+	 * return an iterator for the transaction collection
+	 * 
+	 * @return
+	 */
 	public Iterator<Transaction> iterator() {
 		return membersTransactions.iterator();
 	}
@@ -79,13 +133,8 @@ public class Member implements Serializable {
 	 */
 	@Override
 	public String toString() {
-		String string = "Member name: " + name + ", address: " + address + ", id: " + id + ", date joined: "
+		String string = "Id: " + id + ", member name: " + name + ", address: " + address + ", date joined: "
 				+ dateJoined + ", phone number: " + phoneNumber;
-		if (isFeePaid()) {
-			string += ", Fee has been paid";
-		} else {
-			string += ", Fee has not been paid";
-		}
 
 		return string;
 	}
