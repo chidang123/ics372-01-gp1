858a868fd9c7d7465e56fe786909114f51c7042e
Andrew Vick <atvick21@gmail.com>
2021-03-28 21:30:22 -0500

    Worked on automated testEnrollMember
    Check that it is good. I haven't really worked with assert statements
    before

--- src/store/tests/AutomatedTester.java
+++ src/store/tests/AutomatedTester.java
@@ -1,14 +1,11 @@
 package store.tests;
 
-import store.entities.Product;
-
 import java.util.ArrayList;
 import java.util.Random;
 
 import store.entities.Member;
-import store.entities.Order;
+import store.entities.Product;
 import store.entities.Store;
-import store.ui.UserInterface;
 
 /**
  * This class generates sample automated tests for the store system using
@@ -31,13 +28,20 @@ public class AutomatedTester {
 	AutomatedTester() {
 		this.store = Store.instance();
 	}
+
 	/**
 	 * Tests Member creation.
 	 */
 	public void testEnrollMember() {
-		for ( int enrollIndex = 0; enrollIndex < names.length; enrollIndex++ ) {
-			store.enrollMember(names[enrollIndex], addresses[enrollIndex],
-			phones[enrollIndex]);		
+		for (int count = 0; count < members.length; count++) {
+			members[count].setAddress(addresses[count]);
+			members[count].setName(names[count]);
+			members[count].setPhoneNumber(phones[count]);
+			String memberId = store.enrollMember(addresses[count], names[count], phones[count]);
+			assert members[count].getName().equals(names[count]);
+			assert members[count].getAddress().equals(addresses[count]);
+			assert members[count].getPhoneNumber().equals(phones[count]);
+			assert memberId.equals("XM" + Integer.toString(count));
 		}
 
 	}
@@ -46,56 +50,52 @@ public class AutomatedTester {
 		testEnrollMember();
 		store.removeMember("XM1");
 	}
-	
+
 	/*
-	 * Test store.addProduct and ensure that new product is present
-	 * in 
+	 * Test store.addProduct and ensure that new product is present in
 	 */
 	public void testAddProduct() {
 		final int INITIAL_INVENTORY = 10;
 		final int REORDER_THRESHOLD = 8;
 		Random random = new Random();
-		
-		Product checkoutProduct = store.addProduct(
-		Integer.toString(random.nextInt()), "Dulce de Leche", 7.99,
-		INITIAL_INVENTORY, REORDER_THRESHOLD);
+
+		Product checkoutProduct = store.addProduct(Integer.toString(random.nextInt()), "Dulce de Leche", 7.99,
+				INITIAL_INVENTORY, REORDER_THRESHOLD);
 		assert store.listAllProducts().contains(checkoutProduct.getName());
 	}
-	
+
 	/*
-	 * Test store.checkOut to ensure that order is added
-	 * and that inventory is updated.
+	 * Test store.checkOut to ensure that order is added and that inventory is
+	 * updated.
 	 */
 	public void testCheckout() {
 		final int INITIAL_INVENTORY = 10;
 		final int REORDER_THRESHOLD = 8;
 		final int ORDER_QUANTITY = 3;
-		
+
 		ArrayList<String> productIds = new ArrayList<String>();
 		ArrayList<Integer> productQtys = new ArrayList<Integer>();
 		Random random = new Random();
 
-		String memberString = store.enrollMember(
-		"Nathan Souer", "Maplewood, MN", "6515555555");
-		Product checkoutProduct = store.addProduct(
-		Integer.toString(random.nextInt()), "Alfajores", 4.99,
-		INITIAL_INVENTORY, REORDER_THRESHOLD);
+		String memberString = store.enrollMember("Nathan Souer", "Maplewood, MN", "6515555555");
+		Product checkoutProduct = store.addProduct(Integer.toString(random.nextInt()), "Alfajores", 4.99,
+				INITIAL_INVENTORY, REORDER_THRESHOLD);
 		productIds.add(checkoutProduct.getID());
 		productQtys.add(ORDER_QUANTITY);
 		store.checkout(memberString, productIds, productQtys);
 		assert store.listOutstandingOrders().contains(checkoutProduct.getName());
-		assert checkoutProduct.getInventory() ==  (INITIAL_INVENTORY - ORDER_QUANTITY);
+		assert checkoutProduct.getInventory() == (INITIAL_INVENTORY - ORDER_QUANTITY);
 	}
+
 	public void testProcessShipment() {
-		
-	}	
-	
+
+	}
+
 	public void testChangePrice() {
 		testAddProduct();
-		store.changePrice(products[0].getID(),
-		products[0].getPrice() + 0.5);
+		store.changePrice(products[0].getID(), products[0].getPrice() + 0.5);
 	}
-	
+
 	public void testAll() {
 		testEnrollMember();
 		testRemoveMember();
